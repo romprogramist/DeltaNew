fadc99627110f1c6c61aec0d1c117473219467d6
roman <romprogramist@gmail.com>
2023-06-09 19:53:34 +0400

    new w

--- Delta/Controllers/HomeController.cs
+++ Delta/Controllers/HomeController.cs
@@ -397,7 +397,16 @@ public class HomeController : Controller
     {
         return View();
     }
-
+    public IActionResult GeneralLaboratoryConsumable()
+    {
+        return View();
+    }
+    public IActionResult GeneralLaboratoryConsumableVacutainers()
+    {
+        return View();
+    }
+    
+    
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
